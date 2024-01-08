05cf6e8fd6780eafe61a0d3ca4f23fba6c6d698d
Pushpraj Kushwaha <77253322+Itsmrhashtag@users.noreply.github.com>
2024-01-08 13:40:27 +0530

    Add files via upload

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: ["<span> Hi, I am Pushpraj Kushwaha </span> Working in Back End Development , Enthusiastic Technology with a demonstrated history of learning in the information technology and services industry. Skilled in Back End Tech, JAVA , Web development with multiple tools and technologies . B.Tech focused in Computer Science from Gyan ganga intitute of technology and science , Jabalpur MP "],
-      typeSpeed: 01,
+      typeSpeed: 1,
       contentType: 'html'
       
     });
